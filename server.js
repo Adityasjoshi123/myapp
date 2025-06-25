f01828aa87ab629c92df87518325f4c10711c1d5
Ubuntu <ubuntu@ip-172-20-1-237.ap-south-1.compute.internal>
2025-06-25 10:47:33 +0000

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.end('Hello from my own Node.js app!');
+    res.end('Hello from my own Node.js app!, Remember messi is the goat);
 });
 server.listen(3000, () => {
     console.log('Server running on port 3000');
