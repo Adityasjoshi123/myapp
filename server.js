0b38d215bec1e3bc12676ade77beaf02c581a0c2
Ubuntu <ubuntu@ip-172-20-1-237.ap-south-1.compute.internal>
2025-07-03 08:06:49 +0000

    replaced goat with G.O.A.T

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.end('Hello from my own Node.js app!,aditya is the goat');
+    res.end('Hello from my own Node.js app!,aditya is the G.O.A.T');
 });
 server.listen(3000, () => {
     console.log('Server running on port 3000');
